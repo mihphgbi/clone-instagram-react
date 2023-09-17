a18c20808343659b196c7947c9c9834730aa2150
Minh Phuong <83202708+mihphgbi@users.noreply.github.com>
2023-09-17 19:17:46 +0700

    INS-4 - Add Logo for list menu

--- src/component/NavBar/Nav.js
+++ src/component/NavBar/Nav.js
@@ -1,60 +1,67 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import {connect} from "react-redux";
-import {Box, CardMedia, Grid, ListItemIcon, ListItemText, MenuItem, MenuList} from "@mui/material";
+import {Box, CardMedia, Grid, ListItemButton, ListItemIcon, ListItemText, MenuItem, MenuList} from "@mui/material";
 import logo from "../../assets/Instagram_logo.svg";
+import home from "../../assets/home_icon.svg";
+import search from "../../assets/Search.svg";
 //style
 import './nav-style.scss';
 
 function Nav () {
+    const [selectedItem, setSelectedItem] = useState();
+    const handleClick = (value) => {
+        console.log("========")
+        setSelectedItem(value)
+    }
     return (
         <Fragment>
             <Grid container>
                 <Grid container item xs={12}>
-                    <Box class={'logo-box'}>
+                    <Box className={'logo-box'}>
                         <CardMedia title={'logo'}
                                    image={logo}
                                    sx={{width:'100px', height:'30px'}}/>
                     </Box>
                 </Grid>
                 <Grid container item xs={12}>
-                    <MenuList>
-                        <MenuItem>
+                    <MenuList className={'menu-wrapper'}>
+                        <MenuItem className={'menu-item'} onClick={handleClick} selected={selectedItem}>
                             <ListItemIcon>
-                                <CardMedia title={'Home'}/>
+                                <CardMedia title={'Home'} image={home} sx={{width:'22px', height:'22px'}}/>
                             </ListItemIcon>
                             <ListItemText>Home</ListItemText>
                         </MenuItem>
-                        <MenuItem>
+                        <MenuItem className={'menu-item'} onClick={handleClick} selected={selectedItem}>
                             <ListItemIcon>
-                                A
+                                <CardMedia title={'Home'} image={search} sx={{width:'22px', height:'22px'}}/>
                             </ListItemIcon>
                             <ListItemText>Search</ListItemText>
                         </MenuItem>
-                        <MenuItem>
+                        <MenuItem className={'menu-item'} onClick={handleClick} selected={selectedItem}>
                             <ListItemIcon>
                                 A
                             </ListItemIcon>
                             <ListItemText>Explore</ListItemText>
                         </MenuItem>
-                        <MenuItem>
+                        <MenuItem className={'menu-item'} onClick={handleClick} selected={selectedItem}>
                             <ListItemIcon>
                                 A
                             </ListItemIcon>
                             <ListItemText>Messages</ListItemText>
                         </MenuItem>
-                        <MenuItem>
+                        <MenuItem className={'menu-item'} onClick={handleClick} selected={selectedItem}>
                             <ListItemIcon>
                                 A
                             </ListItemIcon>
                             <ListItemText>Notifications</ListItemText>
                         </MenuItem>
-                        <MenuItem>
+                        <MenuItem className={'menu-item'} onClick={handleClick} selected={selectedItem}>
                             <ListItemIcon>
                                 A
                             </ListItemIcon>
                             <ListItemText>Create</ListItemText>
                         </MenuItem>
-                        <MenuItem>
+                        <MenuItem className={'menu-item'} onClick={handleClick} selected={selectedItem}>
                             <ListItemIcon>
                                 A
                             </ListItemIcon>
